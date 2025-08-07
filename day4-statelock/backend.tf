bdfcca9b030cd1add200c1299a40eebfb412ba2e
Nagaraju <nagarajucivillbrce123@gmail.com>
2025-08-07 17:44:31 +0530

    day4

--- day4-statelock/backend.tf
+++ day4-statelock/backend.tf
@@ -3,5 +3,6 @@ backend "s3" {
 bucket = "nagarajunareshit"
 key = "day4/terraform.tfstate"
 region = "us-east-1"
+ #use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
 }
 }
