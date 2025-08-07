0bb5f144e3a2f3b8996dfe30969e3341fedf06dd
Nagaraju <nagarajucivillbrce123@gmail.com>
2025-08-07 18:03:31 +0530

    day4

--- day4-statelock/backend.tf
+++ day4-statelock/backend.tf
@@ -4,5 +4,7 @@ bucket = "nagarajunareshit"
 key = "day4/terraform.tfstate"
 region = "us-east-1"
  #use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
+  dynamodb_table = "test"  #any version 
+    encrypt = true
 }
 }
--- day4-statelock/main.tf
+++ day4-statelock/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "name" {
     ami = "ami-084a7d336e816906b"
-    instance_type = "t2.nano"
+    instance_type = "t2.micro"
     tags = {
-      Name = "dev"
+      Name = "nag"
     }
   
 }
