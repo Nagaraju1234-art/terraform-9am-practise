4b0758b859e14a0bcb4434a78b5f78b81bf901f6
Nagaraju <nagarajucivillbrce123@gmail.com>
2025-08-29 16:01:11 +0530

    day13

---
+++ day13-vpc-peering/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  
+}
+resource "aws_vpc_peering_connection" "foo" {
+  peer_vpc_id   = aws_vpc.vpca.id
+  vpc_id        = aws_vpc.vpcb.id
+  auto_accept = true   # <--- Accept automatically
+}
+
+resource "aws_vpc" "vpca" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_vpc" "vpcb" {
+  cidr_block = "10.2.0.0/16"
+}
